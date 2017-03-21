acae223d3fbd78a4cc08cb30f30445c92acaec4b
zhengweifu_window <zhengwefu2016@gmail.com>
2017-03-21 17:54:20 +0800

    Mod: common.js absolute path

--- common.js
+++ common.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const path = require('path');
 
 exports = module.exports = {
 	GetConfig: function() {
-		let data = fs.readFileSync('./config.json', 'utf8');
+		let data = fs.readFileSync(path.resolve(__dirname, 'config.json'), 'utf8');
 		return JSON.parse(data.toString('utf8'));
 	}
 }
