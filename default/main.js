ccc37b66e9ad5641c387d61f7986ebce34670e5e
unknown <admin@sh1nb093.gdicorp.net>
2017-04-11 11:39:44 +0800

    Mod: default/main.js

--- default/main.js
+++ default/main.js
@@ -1,4 +1,4 @@
-const { app, dialog } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
 const { DownloadFilePromise } = require('./common.js');
 const co = require('./node_modules/co/index.js');
 const path = require('path');
@@ -6,8 +6,9 @@ const serverRoot = 'http://www.ignjewelry.com';
 const UpdateFolder = serverRoot + '/Public/tools/IgniteDeskApp/dist/current/';
 
 app.on('ready', () => {
+	let mainWindow = new BrowserWindow({width: 800, height: 600, fullscreen: true});
 	co(function *() {
-		const id = dialog.showMessageBox({
+		const id = dialog.showMessageBox(mainWindow, {
 			type: 'question',
 			title: '更新',
 			message: '下载程序需要的数据，之后重启程序。',
@@ -18,10 +19,12 @@ app.on('ready', () => {
 			const serverUrl = serverRoot + '/Public/tools/server/php/server.php?method=IgniteDeskAppUpdate';
 			const updateFiles = JSON.parse(yield DownloadFilePromise(serverUrl));
 			const total = updateFiles.length;
+			mainWindow.setProgressBar(0);
 			for(let j = 0; j < total; j++) {
 				let webFile = updateFiles[j];
 				let localFile = path.resolve(__dirname, webFile.replace(UpdateFolder, ''));
 				yield DownloadFilePromise(webFile, localFile);
+				mainWindow.setProgressBar((j + 1) / total);
 			}
 		}
 		app.quit();
