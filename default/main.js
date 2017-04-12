a297c945d20f4a5435741838b1d313242933a180
zhengweifu <zhengweifu@ign.vip>
2017-04-12 10:28:30 +0800

    Add: v1.1.1

--- default/main.js
+++ default/main.js
@@ -1,32 +1,45 @@
-const { app, BrowserWindow, dialog } = require('electron');
+const { app, BrowserWindow, dialog, ipcMain } = require('electron');
 const { DownloadFilePromise } = require('./common.js');
 const co = require('./node_modules/co/index.js');
 const path = require('path');
 const serverRoot = 'http://www.ignjewelry.com';
 const UpdateFolder = serverRoot + '/Public/tools/IgniteDeskApp/dist/current/';
 
+function *downloadDatas(_window, _config) {
+	const id = dialog.showMessageBox(_window, {
+		type: 'question',
+		title: '更新',
+		message: '下载程序需要的数据，之后重启程序。',
+		cancelId: -1,
+		buttons: ['确定']
+	});
+	if(id == 0) {
+		const serverUrl = serverRoot + '/Public/tools/server/php/server.php?method=IgniteDeskAppUpdate';
+		const updateFiles = JSON.parse(yield DownloadFilePromise(serverUrl));
+		const total = updateFiles.length;
+		_window.setProgressBar(0);
+		for(let j = 0; j < total; j++) {
+			let webFile = updateFiles[j];
+			let localFile = path.resolve(__dirname, webFile.replace(UpdateFolder, ''));
+			yield DownloadFilePromise(webFile, localFile);
+			_window.setProgressBar((j + 1) / total);
+		}
+	}
+	app.quit();
+};
+
+let isOnLine = false, timer;
 app.on('ready', () => {
 	let mainWindow = new BrowserWindow({width: 800, height: 600, fullscreen: true});
-	co(function *() {
-		const id = dialog.showMessageBox(mainWindow, {
-			type: 'question',
-			title: '更新',
-			message: '下载程序需要的数据，之后重启程序。',
-			cancelId: -1,
-			buttons: ['确定']
-		});
-		if(id == 0) {
-			const serverUrl = serverRoot + '/Public/tools/server/php/server.php?method=IgniteDeskAppUpdate';
-			const updateFiles = JSON.parse(yield DownloadFilePromise(serverUrl));
-			const total = updateFiles.length;
-			mainWindow.setProgressBar(0);
-			for(let j = 0; j < total; j++) {
-				let webFile = updateFiles[j];
-				let localFile = path.resolve(__dirname, webFile.replace(UpdateFolder, ''));
-				yield DownloadFilePromise(webFile, localFile);
-				mainWindow.setProgressBar((j + 1) / total);
-			}
+	mainWindow.loadURL('file://' + __dirname + '/online_status.html');
+	timer = setInterval(() => {
+		if(isOnLine) {
+			co(downloadDatas(mainWindow));
+			clearInterval(timer);
 		}
-		app.quit();
-	});
+	}, 1000);
+});
+
+ipcMain.on('online-status-changed', (event, status) => {
+	isOnLine = status;
 });
--- main.js
+++ main.js
@@ -1,46 +1,57 @@
-const {app, BrowserWindow, dialog} = require('electron');
+const { app, BrowserWindow, dialog, ipcMain } = require('electron');
 const { GetConfig, DownloadFilePromise } = require('./common.js');
 const path = require('path');
 const co = require('./node_modules/co/index.js');
 let config = GetConfig();
 const http = require('http');
 
-const UpdateFolder = config.serverRoot + '/Public/tools/IgniteDeskApp/dist/current/';
-let mainWindow;
+let isOnLine = false;
+
+let mainWindow, timer;
+
+function *downloadDatas(_window, _config) {
+	const UpdateFolder = config.serverRoot + '/Public/tools/IgniteDeskApp/dist/current/';
+	const currentVersion = app.getVersion();
+	const webPackage = JSON.parse(yield DownloadFilePromise(UpdateFolder + 'package.json'));
+	const webVersion = webPackage.version;
+	if(currentVersion != webVersion && _config.checkUpdate) {
+		const id = dialog.showMessageBox(_window, {
+			type: 'question',
+			title: '更新',
+			cancelId: 1,
+			message: '发现有新的版本可以更新，单击确定按钮更新，之后重启程序。',
+			buttons: ['确定', '取消']
+		});
+		if(id == 0) {
+			const serverUrl = _config.serverRoot + '/Public/tools/server/php/server.php?method=IgniteDeskAppUpdate';
+			const updateFiles = JSON.parse(yield DownloadFilePromise(serverUrl));
+			const total = updateFiles.length;
+			_window.setProgressBar(0);
+			for(let j = 0; j < total; j++) {
+				let webFile = updateFiles[j];
+				let localFile = path.resolve(__dirname, webFile.replace(UpdateFolder, ''));
+				yield DownloadFilePromise(webFile, localFile);
+				_window.setProgressBar((j + 1) / total);
+			}
+
+			app.quit();
+		}
+	}
+
+	_window.loadURL('file://' + __dirname + '/index.html');
+	_window.setMenu(null);
+	if(_config.showDevTools) _window.webContents.openDevTools();
+};
+
 app.on('ready', () => {
 	mainWindow = new BrowserWindow({width: 800, height: 600, fullscreen: true});
-	co(function *() {
-		const currentVersion = app.getVersion();
-		const webPackage = JSON.parse(yield DownloadFilePromise(UpdateFolder + 'package.json'));
-		const webVersion = webPackage.version;
-		if(currentVersion != webVersion && config.checkUpdate) {
-			const id = dialog.showMessageBox(mainWindow, {
-				type: 'question',
-				title: '更新',
-				cancelId: 1,
-				message: '发现有新的版本可以更新，单击确定按钮更新，之后重启程序。',
-				buttons: ['确定', '取消']
-			});
-			if(id == 0) {
-				const serverUrl = config.serverRoot + '/Public/tools/server/php/server.php?method=IgniteDeskAppUpdate';
-				const updateFiles = JSON.parse(yield DownloadFilePromise(serverUrl));
-				const total = updateFiles.length;
-				mainWindow.setProgressBar(0);
-				for(let j = 0; j < total; j++) {
-					let webFile = updateFiles[j];
-					let localFile = path.resolve(__dirname, webFile.replace(UpdateFolder, ''));
-					yield DownloadFilePromise(webFile, localFile);
-					mainWindow.setProgressBar((j + 1) / total);
-				}
-
-				app.quit();
-			}
+	mainWindow.loadURL('file://' + __dirname + '/online_status.html');
+	timer = setInterval(() => {
+		if(isOnLine) {
+			co(downloadDatas(mainWindow, config));
+			clearInterval(timer);
 		}
-
-		mainWindow.loadURL('file://' + __dirname + '/index.html');
-		mainWindow.setMenu(null);
-		if(config.showDevTools) mainWindow.webContents.openDevTools();
-	});
+	}, 1000);
 });
 
 app.on('window-all-closed', () => {
@@ -49,7 +60,6 @@ app.on('window-all-closed', () => {
 	}
 });
 
-// app.on('will-quit', () => {
-// 	console.log(electron);
-// });
-
+ipcMain.on('online-status-changed', (event, status) => {
+	isOnLine = status;
+});
